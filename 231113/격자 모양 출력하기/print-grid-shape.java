6f7a9d985217939786972e89b12a04e69c4f64a7
Bruce Han <intragerhan@gmail.com>
2023-11-13 20:36:38 +0900

    Title: 격자 모양 출력하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231113/격자 모양 출력하기/print-grid-shape.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+    
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[r-1][c-1] = r * c;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
