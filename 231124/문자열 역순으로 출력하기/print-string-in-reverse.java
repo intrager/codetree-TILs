1c4355bead030a15164ae42e508f5cbacb922345
Bruce Han <intragerhan@gmail.com>
2023-11-24 23:24:05 +0900

    Title: 문자열 역순으로 출력하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231124/문자열 역순으로 출력하기/print-string-in-reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        String str3 = br.readLine();
+        String str4 = br.readLine();
+    
+        System.out.println(str4);
+        System.out.println(str3);
+        System.out.println(str2);
+        System.out.println(str1);
+    }
+}
