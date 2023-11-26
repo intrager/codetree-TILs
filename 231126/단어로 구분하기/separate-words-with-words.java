d03d697412206e1b39c184543131455d13cb7a63
Bruce Han <intragerhan@gmail.com>
2023-11-26 10:44:28 +0900

    Title: 단어로 구분하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231126/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+        for(String str : arr) {
+            System.out.println(str);
+        }
+    }
+}
