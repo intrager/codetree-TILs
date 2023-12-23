387b07a11ef465bfa985ac49cf572976102f8a5a
Bruce Han <intragerhan@gmail.com>
2023-12-23 11:41:17 +0900

    Title: 두 번째를 첫 번째로, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231223/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        char one = str.charAt(0);
+        char two = str.charAt(1);
+    
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == two) sb.append(one);
+            else sb.append(str.charAt(i));
+        }
+        System.out.print(sb.toString());
+    }
+}
