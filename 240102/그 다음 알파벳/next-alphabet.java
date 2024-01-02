cd3cba928d6c2560245f9aaf898abc482fa1857b
Bruce Han <intragerhan@gmail.com>
2024-01-02 23:54:02 +0900

    Title: 그 다음 알파벳, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240102/그 다음 알파벳/next-alphabet.java
+++ 240102/그 다음 알파벳/next-alphabet.java
@@ -6,6 +6,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int ch = (int)br.readLine().charAt(0) + 1;
+        if(ch > 126) {
+            ch = 97;
+        }
         System.out.print((char)ch);
     }
 }
