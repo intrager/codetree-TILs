9a3465d0506dee8562065894beb8f6b42ee96296
Bruce Han <intragerhan@gmail.com>
2024-01-04 21:43:28 +0900

    Title: 대문자로 출력하기, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240104/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] chArr = br.readLine().toCharArray();
+        StringBuffer sb = new StringBuffer();
+
+        for(int i = 0; i < chArr.length; i++) {
+            if(chArr[i] > 97 && chArr[i] < 122) {
+                char ch = (char)(chArr[i] - 'a' + 'A');
+                sb.append(ch);
+            } else if(chArr[i] > 65 && chArr[i] < 90) {
+                sb.append(chArr[i]);
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
