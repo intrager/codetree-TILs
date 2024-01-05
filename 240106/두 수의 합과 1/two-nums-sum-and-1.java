0182d2b50769457d624b8ac501930efd23845495
Bruce Han <intragerhan@gmail.com>
2024-01-06 01:27:54 +0900

    Title: 두 수의 합과 1, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240106/두 수의 합과 1/two-nums-sum-and-1.java
+++ 240106/두 수의 합과 1/two-nums-sum-and-1.java
@@ -6,11 +6,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] arr = br.readLine().split(" ");
-        String str = (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1])).toString();
+        String str = new Integer(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1])).toString();
 
         int count = 0;
         for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 48) count++;
+            if(str.charAt(i) == 49) count++;
         }
         System.out.print(count);
     }
