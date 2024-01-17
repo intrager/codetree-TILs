a662ac63b03375b1799c8aac47338dbcab15c305
Bruce Han <intragerhan@gmail.com>
2024-01-17 23:40:53 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240117/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
+++ 240117/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] n = br.readLine().split("");
+        String n = br.readLine();
         int sum = 0;
 
-        for(int i = 0; i < n.length; i++) {
-            sum += Integer.parseInt(n[i]);
+        for(int i = 0; i < n.length(); i++) {
+            sum += n.charAt(i) - '0';
         }
-        if(sum % 5 == 0) {
+        if(Integer.parseInt(n) % 2 == 0 && sum % 5 == 0) {
             System.out.print("Yes");
         } else {
             System.out.print("No");
