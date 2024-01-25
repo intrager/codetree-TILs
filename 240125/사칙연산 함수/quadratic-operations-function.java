90384da31cbe73c5b20e20c63bb28d5f4480aa44
Bruce Han <intragerhan@gmail.com>
2024-01-25 19:23:15 +0900

    Title: 사칙연산 함수, Time: 108ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240125/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+        int a = Integer.parseInt(arr[0]);
+        String o = arr[1];
+        int c = Integer.parseInt(arr[2]);
+    
+        String answer = calculate(a, o, c);
+        System.out.print(a + " " + o + " " + c + " = " + answer);
+    }
+
+    private static String calculate(int a, String o, int c) {
+        String result = "";
+        if (o.equals("+")) {
+            result = String.valueOf(a + c);
+        } else if (o.equals("-")) {
+            result = String.valueOf(a - c);
+        } else if (o.equals("/")) {
+            result = String.valueOf(a / c);
+        } else if (o.equals("*")) {
+            result = String.valueOf(a * c);
+        } else {
+            result = "False";
+        }
+        return result;
+    }
+}
