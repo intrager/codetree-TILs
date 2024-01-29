e8771ff80e66e22ca3beb30baa57b22dcabc095c
Bruce Han <intragerhan@gmail.com>
2024-01-29 23:14:08 +0900

    Title: 2021년 날짜의 유무, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 240129/2021년 날짜의 유무/with-or-without-2021.java
+++ 240129/2021년 날짜의 유무/with-or-without-2021.java
@@ -7,16 +7,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int month = Integer.parseInt(st.nextToken());
-        int day = Integer.parseInt(st.nextToken());
-    
-        try {
-            LocalDate date = LocalDate.of(2021, month, day);
-            if(date != null) {
-                System.out.print("Yes");
-            }
-        } catch (DateTimeException e) {
+        int m = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        
+        if(judgeDay(m, d)) {
+            System.out.print("Yes");
+        } else {
             System.out.print("No");
+        }
+    }
+
+    // 윤년이 아닐 때 m월 d일이 존재하는지 여부 확인
+    private static boolean judgeDay(int m, int d) {
+        if(m <= 12 && d <= lastDayNumber(m)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 윤년이 아닐 때 m 번째 달의 마지막 날을 반환
+    private static int lastDayNumber(int m) {
+        if(m == 2) {
+            return 28;
+        } else if (m == 4 || m == 6 || m == 9 || m == 11) {
+            return 30;
+        } else {
+            return 31;
         }
     }
 }
