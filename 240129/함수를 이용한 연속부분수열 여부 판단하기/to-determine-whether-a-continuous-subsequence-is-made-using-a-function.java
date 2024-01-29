d68710ce9e0914738c055eb9b9e49ece176dc6a2
Bruce Han <intragerhan@gmail.com>
2024-01-29 22:22:09 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240129/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+    
+        int[] A = new int[n1];
+        int[] B = new int[n2];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int arrayIndex = 0;
+        while(st.hasMoreTokens()) {
+            A[arrayIndex++] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        arrayIndex = 0;
+        while(st.hasMoreTokens()) {
+            B[arrayIndex++] = Integer.parseInt(st.nextToken());
+        }
+        
+        if(isConsecutiveNumberSubSequence(A, B, n1, n2)) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+    }
+
+    private static boolean isConsecutiveNumberSubSequence(int[] A, int[] B, int n1, int n2) {
+        for(int i = 0; i < n1; i++) {
+            for(int j = 0; j < n2 - 1; j++) {
+                if(B[j] == A[j + i] && B[j + 1] == A[j + 1 + i]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
