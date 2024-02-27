2f5a302b2907ec3332792c2f5c7dc88cdf53ed7d
Bruce Han <intragerhan@gmail.com>
2024-02-27 20:28:26 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 155ms, Memory: 8MB, Status: Passed - CodeTree

--- 240227/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
+++ 240227/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -2,15 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static String text, pattern;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String inputString = br.readLine();
-        String objectString = br.readLine();
-
-        int index = getIndex(inputString, objectString);
-        System.out.print(index);
+        text = br.readLine();
+        pattern = br.readLine();
+        System.out.print(findIndex());
     }
+
+    public static int findIndex() {
+        int textLength = text.length();
+        for(int i = 0; i < textLength; i++) {
+            if(isSubString(i))
+                return i;
+        }
+        return -1;
+    }
+
+    public static boolean isSubString(int startIndex) {
+        int textLength = text.length();
+        int patternLength = pattern.length();
+
+        if(startIndex + patternLength - 1 >= textLength)
+            return false;
+
+        for(int j = 0; j < patternLength; j++) {
+            if(text.charAt(startIndex + j) != pattern.charAt(j))
+                return false;
+        }
+        return true;
+    }
+
+    
 
     public static int getIndex(String base, String object) {
         int index = -1;
