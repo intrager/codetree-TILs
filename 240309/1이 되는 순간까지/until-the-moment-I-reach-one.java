dc6d2eb24e484d8e2c7ff49ebb119cdebfaa1973
Bruce Han <intragerhan@gmail.com>
2024-03-09 09:55:44 +0900

    Title: 1이 되는 순간까지, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 240309/1이 되는 순간까지/until-the-moment-I-reach-one.java
+++ 240309/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -2,24 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static int count = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        calculate(n);
-        System.out.print(count);
+        System.out.print(calculate(n));
     }
 
     private static int calculate(int n) {
         if(n == 1)
-            return 1;
-        count++;
+            return 0;
         if(n % 2 == 0) {
-            return calculate(n / 2);
+            return calculate(n / 2) + 1;
         } else {
-            return calculate(n / 3);
+            return calculate(n / 3) + 1;
         }
     }
 }
