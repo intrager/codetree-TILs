b1c0e52a4d46d1294664bf540409af7d2832242c
Bruce Han <intragerhan@gmail.com>
2024-03-28 21:55:02 +0900

    Title: 문자열 정렬, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 240328/문자열 정렬/string-sort.java
+++ 240328/문자열 정렬/string-sort.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] charArr = br.readLine().split("");
+        char[] charArr = br.readLine().toCharArray();
         Arrays.sort(charArr);
         String answer = new String(charArr);
         System.out.print(answer);
