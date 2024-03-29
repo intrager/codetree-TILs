98d16682d76e618f2cacf3125778045a59e63b70
Bruce Han <intragerhan@gmail.com>
2024-03-29 21:00:34 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240329/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ 240329/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -1,31 +1,43 @@
-import java.util.*;
-import java.io.*;
-import java.util.stream.*;
+import java.util.Arrays;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Main {    
+public class Main {
+    public static final int MAX_SIZE = 10;
+    public static int[] arr = new int[MAX_SIZE + 1];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        
-        int answer = arr[0];
-        for(int i = 1; i < n; i++) {
-            answer = getLCM(answer, arr[i]);
+        String[] strArr = br.readLine().split(" ");
+
+        for(int i = 0; i < MAX_SIZE; i++) {
+            arr[i] = Integer.parseInt(strArr[i]);
         }
-        System.out.print(answer);
+        
+        System.out.print(getLCMAll(n));
+    }
+
+    // index번째까지 인덱스의 숫자 중에 가장 큰 값을 반환한다.
+    private static int getLCMAll(int idx) {
+        // 남은 원소가 1개라면 그 자신이 답이 된다.
+        if(idx == 1)
+            return arr[1];
+        
+        // 1번째 ~ index - 1번째 원소의 최소공배수를 구한 결과와
+        // 현재 index 원소의 최소공배수를 구하여 반환한다.
+        return getLCM(getLCMAll(idx - 1), arr[idx]);
     }
 
     private static int getLCM(int a, int b) {
-        return (a * b) / getGCD(a, b);
+        int gcd = 1;
+        for(int i = 1; i <= Math.min(a, b); i++) {
+            if(a % i == 0 && b % i == 0)
+                gcd = i;
+        }
+        return a * b / gcd;
     }
 
-    private static int getGCD(int a, int b) {
-        while(b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
-        }
-        return a;
-    }
 }
