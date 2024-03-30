d78d7c8c68a4c24446f0354640b50edcdb374c14
Bruce Han <intragerhan@gmail.com>
2024-03-30 09:30:13 +0900

    Title: 단어 정렬, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240330/단어 정렬/sorting-words.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr);
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
