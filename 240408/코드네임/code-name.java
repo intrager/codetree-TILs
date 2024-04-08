e88f9a371b285c87f92cbdc0bf8ef40a75a1c2c5
Bruce Han <intragerhan@gmail.com>
2024-04-08 22:33:34 +0900

    Title: 코드네임, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240408/코드네임/code-name.java
+++ 240408/코드네임/code-name.java
@@ -3,22 +3,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class CodeName {
-    private String name;
-    private int score;
+class User {
+    char codeName;
+    int score;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setScore(int score) {
+    public User(char codeName, int score) {
+        this.codeName = codeName;
         this.score = score;
-    } 
-    public int getScore() {
-        return score;
     }
 }
 
@@ -26,20 +17,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int min = Integer.MAX_VALUE;
-        CodeName code = new CodeName();
+        int len = 5;
+        User[] users = new User[len];
 
-        for(int i = 0; i < 5; i++) {
+        for(int i = 0; i < len; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            String name = st.nextToken();
+            char codeName = st.nextToken().charAt(0);
             int score = Integer.parseInt(st.nextToken());
-            if(score < min) {
-                min = score;
-                code.setName(name);
-                code.setScore(score);
-            }
+
+            users[i] = new User(codeName, score);
         }
 
-        System.out.print(code.getName() + " " + code.getScore());
+        int minIndex = 0;
+
+        for(int i = 1; i < len; i++) {
+            if(users[minIndex].score > users[i].score)
+                minIndex = i;
+        }
+        System.out.print(users[minIndex].codeName + " " + users[minIndex].score);
     }
 }
