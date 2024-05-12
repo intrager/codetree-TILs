b0b273fead5872cb56fc6894595a4f90a8f426d1
Bruce Han <intragerhan@gmail.com>
2024-05-12 19:31:17 +0900

    Title: 요일 맞추기, Time: 116ms, Memory: 8MB, Status: Passed - CodeTree

--- 240512/요일 맞추기/guess-day-of-week.java
+++ 240512/요일 맞추기/guess-day-of-week.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] week = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        String[] week = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         String[] arr = br.readLine().split(" ");
@@ -19,25 +19,21 @@ public class Main {
 
         int days1 = 0;
         int days2 = 0;
-        for(int i = 0; i < m1; i++) {
+        for(int i = 1; i < m1; i++) {
             days1 += days[i];
         }
         days1 += d1;
 
-        for(int i = 0; i < m2; i++) {
+        for(int i = 1; i < m2; i++) {
             days2 += days[i];
         }
         days2 += d2;
 
 
-        int answer = 0;
-        if(days2 > days1) {
-            answer = (days2 - days1) % 7 + 1;
-        } else if(days2 < days1) {
-            answer = (days1 - days2) % 6;
-        } else {
-            answer = (days2 - days1) % 7 + 1;
-        }
+        int answer = days2 - days1;
+        while(answer < 0)
+            answer += 7;
+        answer %= 7;
         System.out.print(week[answer]);
     }
 }
