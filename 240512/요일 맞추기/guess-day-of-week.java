23538f52fcc108ceb6112ecee1155d387302b108
Bruce Han <intragerhan@gmail.com>
2024-05-12 19:19:56 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240512/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] week = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        String[] arr = br.readLine().split(" ");
+
+        int m1 = Integer.parseInt(arr[0]);
+        int d1 = Integer.parseInt(arr[1]);
+        int m2 = Integer.parseInt(arr[2]);
+        int d2 = Integer.parseInt(arr[3]);
+    
+
+        int days1 = 0;
+        int days2 = 0;
+        for(int i = 0; i < m1; i++) {
+            days1 += days[i];
+        }
+        days1 += d1;
+
+        for(int i = 0; i < m2; i++) {
+            days2 += days[i];
+        }
+        days2 += d2;
+
+        int answer = (days2 - days1) % 7 + 1;
+        System.out.print(week[answer]);
+    }
+}
