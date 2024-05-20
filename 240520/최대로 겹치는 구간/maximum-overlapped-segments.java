6e73ebd9ef8fd8902088a24a1702d26aa8ee955d
Bruce Han <intragerhan@gmail.com>
2024-05-20 20:58:15 +0900

    Title: 최대로 겹치는 구간, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240520/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240520/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,39 +1,43 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static final int MAX_R = 200;
+    public static final int OFFSET = 100;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 100];
 
-        for(int j = 0; j < n; j++) {
-            String[] a = br.readLine().split(" ");
-            int x1 = Integer.parseInt(a[0]);
-            int x2 = Integer.parseInt(a[1]);
-
-            for(int i = x1; i < x2; i++) {
-                arr[i]++;
+        int[] x1 = new int[MAX_N];
+        int[] x2 = new int[MAX_N];
+        
+        int[] checked = new int[MAX_R + 1];
+        
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            x1[i] = Integer.parseInt(st.nextToken());
+            x2[i] = Integer.parseInt(st.nextToken());
+        
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = x1[i]; j < x2[i]; j++) {
+                checked[j]++;
             }
         }
 
-        int cnt = n + 100;
         int max = 0;
-        for(int i = 0; i < arr.length; i++) {
-            if(max < arr[i]) {
-                cnt = Math.min(cnt, i);
-                max = Math.max(max, arr[i]);
+        for(int i = 0; i <= MAX_R; i++) {
+            if(max < checked[i]) {
+                max = checked[i];
             }
         }
-
-        int answer = 0;
-        for(int i = cnt; i < arr.length; i++) {
-            if(max == arr[i]) {
-                answer++;
-            }
-        }
-        System.out.print(answer - 1);
+        System.out.print(max);
     }
 }
